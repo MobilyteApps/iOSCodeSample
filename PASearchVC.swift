bc9dbab5ce847ada9781aab1e5219d2040e7d50a
MobilyteApps <39619790+MobilyteApps@users.noreply.github.com>
2021-01-15 02:39:26 +0530

    Update PASearchVC.swift

--- PASearchVC.swift
+++ PASearchVC.swift
@@ -2,8 +2,8 @@
 //  PASearchVC.swift
 //  Projectart
 //
-//  Created by Sunil Garg on 10/08/19.
-//  Copyright © 2019 Sunil Garg. All rights reserved.
+//  Created by Infostride on 10/08/19.
+//  Copyright © 2019 Infostride. All rights reserved.
 //
 
 import UIKit
